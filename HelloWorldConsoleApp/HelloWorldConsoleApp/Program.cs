666dde2a938176d34c79e1c5483498c79d7375dc
Kyralesova <109028438+Kyralesova@users.noreply.github.com>
2022-07-17 20:02:19 +0300

    Add files via upload

---
+++ HelloWorldConsoleApp/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+   
+    class Program
+    {
+        /// <summary>
+        /// Код который выполняется в первую очередь
+        /// </summary>
+        /// <param name="args"> Параметры запуска </param>>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!"); // Вывод текста
+                                               // в консоль
+
+            Console.ReadKey(); // Ожидание от пользователя
+                               // нажатия какой-либо кнопки
+        }
+    }
+}
